887a05c35713c0e2ab508d2bd459ddb62e326316
Rosa Fernandes <rosferna@student.42lisboa.com>
2021-12-02 20:01:04 +0000

    done with 2 static

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,8 @@
 #include<sys/stat.h>
 #include <fcntl.h>
 
- !!! ESTAS A USAR A FUNCAO STRLEN ORIGINAL - MUDA PARA FT_STRLEN
-// 
-//
-//
-char	*ft_strjoin(char *s1, char *s2)
+//esta função concatena s1 com n bytes de s2
+char	*ft_strnjoin(char *s1, char *s2, size_t n)
 {
 	char	*new;
 	int		i;
@@ -18,68 +15,62 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	if (!s1 || !s2)
 		return (0);
-	new = (char *)malloc(strlen(s1) + strlen(s2) + 1 * sizeof(char));
+	i = 0;
+	while (s1[i])
+		i++;
+	new = (char *)malloc(i + n + 1 * sizeof(char));
 	if (!new)
 		return (0);
 	i = 0;
-	j = 0;
 	while (s1[i] != '\0')
-	{
-		new[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		new[i] = s2[j];
-		j++;
-		i++;
-	}
+		new[i++] = s1[i];
+	j = 0;
+	while (j != n)
+		new[i++] = s2[j++];
 	new[i] = '\0';
 	return (new);
 }
 
 char *get_next_line(int fd)
 {
-    static char *reference = '\0';
+    static char *reference = "\0";
     char *substring = "";
     size_t end = 0;
-	static char flag[] = "0";
+	static int flag = 0;
 	
-	// verificar se o ficheiro existe OU se a referencia aponta para o final do ficheiro
-    if (fd < 0)
-        return (0);
-	
-	// ler o ficheiro SE o conteudo da referencia for o NULL
-    if (*reference == '\0')
+    if (fd < 0 || BUFFER_SIZE < 0)
+        return (NULL);
+    while (flag == 0)
 	{
-		//apagar o conteudo da referencia e atribuir novo valor
-		free(reference);
-		reference = (char *)malloc(BUFFER_SIZE);
-		if (!reference)
-			return (0);
-        end = read(fd, reference, BUFFER_SIZE);
-		//se estivermos no final do ficheiro mudar o valor da flag
-		if (end != BUFFER_SIZE)
-			*flag = '1';
-		reference[end] = '\0';
-	}
-
-	//enquanto nao chegar ao final do ficheiro
-	end = 0;
-    while (reference[end] != '\0')
-    {
-		//se encontrar uma quebra de libra
-        if (reference[end] == '\n' || reference[end + 1] == '\0')
+		if (*reference == '\0')
 		{
-            substring = ft_strjoin(substring, reference);
-			if (reference[end + 1] == '\0' && *flag != 1) 
-				//ler outra vez o ficheiro
-			else
-				reference = reference + (end + 1);
-			return (substring);
+			reference = (char *)malloc(BUFFER_SIZE + 1);
+			if (!reference)
+				return (0);
+        	end = read(fd, reference, BUFFER_SIZE);
+			if (end == 0)
+			{
+				flag = flag + 1;
+				return (substring);
+			}
+			reference[end] = '\0';
 		}
-		end++;
-    }
+		end = 0;
+    	while (reference[end] != '\0')
+    	{
+        	if (reference[end] == '\n')
+			{
+           		substring = ft_strnjoin(substring, reference, (end + 1));
+				reference = reference + (end + 1);
+				return (substring);	
+			}
+			end++;
+    	}
+		substring = ft_strnjoin(substring, reference, end);
+		reference = reference + end;
+	}
+	return (NULL);
+}
 
 int main(void)
 {
@@ -87,10 +78,11 @@ int main(void)
 	char *line;
 	int i = 0;
 	fd = open("texto.txt", O_RDONLY);
-	while (i < 1)
+	while (i < 8)
 	{
 		line = get_next_line(fd);
 		printf("%s" , line);
 		i++;
 	}
+	//printf("%s", line);
 }
