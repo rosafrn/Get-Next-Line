4dbee9aac3ef2feb36fa04dc4c45d45292af2568
Rosa Fernandes <rosferna@student.42lisboa.com>
2021-12-29 17:32:36 +0000

    sem erro

--- get_next_line.c
+++ get_next_line.c
@@ -12,11 +12,11 @@
 
 #include "get_next_line.h"
 
-void ft_concat(char **reference, char **line, size_t end)
+void	ft_concat(char **reference, char **line, size_t end)
 {
-	char *temp = NULL;
+	char	*temp;
 
-    temp = ft_strnjoin(*line, *reference, end);
+	temp = ft_strnjoin(*line, *reference, end);
 	free(*line);
 	*line = temp;
 	if (reference[0][end - 1] == '\n')
@@ -32,23 +32,23 @@ void ft_concat(char **reference, char **line, size_t end)
 	}
 }
 
-char *ft_read(int fd, char **reference, char **line, size_t end, int *flag)
+char	*ft_read(int fd, char **reference, char **line, int *flag)
 {
+	int	end;
+
 	*reference = (char *)malloc(BUFFER_SIZE + 1);
 	if (!*reference)
 		return (NULL);
-    end = read(fd, *reference, BUFFER_SIZE);
+	end = read(fd, *reference, BUFFER_SIZE);
 	if (end == 0)
 	{
 		free (*reference);
 		*flag = *flag + 1;
-		if (!*line)
-			return (NULL);
-		if (**line == '\0')
+		if (!*line || **line == '\0')
 		{
-			free (*line);
+			free(*line);
 			return (NULL);
-		}
+		}	
 		return (*line);
 	}
 	if (end < 0)
@@ -60,40 +60,53 @@ char *ft_read(int fd, char **reference, char **line, size_t end, int *flag)
 	return (*reference);
 }
 
-char *get_next_line(int fd)
+void	blabla(char **reference, char **line)
 {
-    static char *reference = NULL;
-    char *line = NULL;
-    int end;
-	static int flag = 0;
-	
-    if (fd < 0 || fd > 1024 || read(fd, 0, 0) == -1 || BUFFER_SIZE <= 0)
-        return (NULL);
-    while (flag == 0)
+	int	end;
+
+	end = 0;
+	while ((*reference)[end] != '\0')
+	{
+		if ((*reference)[end] == '\n')
+		{
+			if (!(*line))
+				*line = ft_strdup("");
+			ft_concat(&*reference, &*line, (end + 1));
+			end = -1;
+			break ;
+		}
+		end++;
+	}
+	if (end != -1)
+	{
+		if (!(*line))
+			*line = ft_strdup("");
+		ft_concat(&*reference, &*line, end);
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*reference = NULL;
+	char		*line;
+	static int	flag = 0;
+
+	line = NULL;
+	if (fd < 0 || fd > 1024 || read(fd, 0, 0) == -1 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (flag == 0)
 	{
 		if (reference == NULL)
 		{
-			reference = ft_read(fd, &reference, &line, end, &flag);
+			reference = ft_read(fd, &reference, &line, &flag);
 			if (!reference)
 				return (NULL);
 			if (flag == 1)
 				return (reference);
 		}
-		end = 0;
-    	while (reference[end] != '\0')
-    	{
-        	if (reference[end] == '\n')
-			{
-				if (!line)
-					line = ft_strdup("");
-				ft_concat(&reference, &line, (end + 1));
-				return (line);	
-			}
-			end++;
-    	}
-		if (!line)
-			line = ft_strdup("");
-		ft_concat(&reference, &line, end);
+		blabla(&reference, &line);
+		if (reference != NULL)
+			return (line);
 	}
 	return (NULL);
 }
@@ -111,12 +124,5 @@ char *get_next_line(int fd)
 // 		i++;
 // 	}
 // 	//printf("%s", line);
-// }
-
-// int main(void)
-// {
-// 	int fd = -4;//open("nl", O_RDONLY);
-
-// 	printf("%s", get_next_line(fd));
 // }
 
