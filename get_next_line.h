f4ec6a9f926d69846cfb57df176fef3ff653c29c
Rosa Fernandes <rosferna@student.42lisboa.com>
2021-12-29 17:39:23 +0000

    solve norminette

--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosferna <rosferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 17:38:06 by rosferna          #+#    #+#             */
+/*   Updated: 2021/12/29 17:38:31 by rosferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>
 
 # define FD_MAX 65534
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
-char *get_next_line(int fd);
-char * ft_strdup(const char *s);
-char *ft_substr(char *s, size_t i);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char *s, size_t i);
 char	*ft_strnjoin(char *s1, char *s2, size_t n);
-
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,24 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosferna <rosferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 17:36:49 by rosferna          #+#    #+#             */
+/*   Updated: 2021/12/29 17:37:53 by rosferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 //esta função aloca memoria para a string s1 e atribui o valor ""
-char * ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	char *str;
+	char	*str;
 
 	str = malloc(sizeof(char) * 2);
 	if (!str)
 		return (NULL);
 	str[0] = *s;
 	str[1] = '\0';
-	return(str);
+	return (str);
 }
 
-//esta função cria uma substring de s iniciando no index i passado como parametro
-char *ft_substr(char *s, size_t i)
+//esta função cria uma substring de s
+//  iniciando no index i passado como parametro
+char	*ft_substr(char *s, size_t i)
 {
-	char *str;
-	size_t x = 0;
+	char	*str;
+	size_t	x;
 
+	x = 0;
 	if (!s)
 		return (0);
 	str = malloc(sizeof(char) * ((strlen(s) - i) + 1));
@@ -31,16 +45,15 @@ char *ft_substr(char *s, size_t i)
 		i++;
 	}
 	str[x] = '\0';
-	return(str);
-
-
+	return (str);
 }
+
 //esta função concatena s1 com n bytes de s2
 char	*ft_strnjoin(char *s1, char *s2, size_t n)
 {
 	char	*new;
-	size_t		i;
-	size_t		j;
+	size_t	i;
+	size_t	j;
 
 	if (!s1 || !s2)
 		return (0);
